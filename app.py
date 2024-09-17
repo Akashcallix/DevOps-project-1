d58ed67090acd3bf90178348d4597a85446accc4
Akashdeep singh <akashdeep.singh@noi-dc01.mcarbon.com>
2024-09-17 23:28:42 +0530

    CSS added

--- app.py
+++ app.py
@@ -1,19 +1,33 @@
-from flask import Flask
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return "Welcome to my Flask App!"
+
+# @app.route('/about')
+# def about():
+#     return "This is a simple DevOps project to showcase CI/CD and Docker."
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
+
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to my Flask App!"
+    return render_template('index.html')
 
 @app.route('/about')
 def about():
-    return "This is a simple DevOps project to showcase CI/CD and Docker."
+    return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
-
-
+    app.run(debug=True)
 
 
 # from flask import Flask
