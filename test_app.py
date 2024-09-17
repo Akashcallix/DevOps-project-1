94120fd5068ffe179a5888ac84be835ab985f935
Akashdeep singh <akashdeep.singh@noi-dc01.mcarbon.com>
2024-09-17 17:31:05 +0530

    final test and CI

--- test_app.py
+++ test_app.py
@@ -1,14 +1,32 @@
-
-# test_app.py
 import pytest
 from app import app
 
 @pytest.fixture
 def client():
+    app.config['TESTING'] = True
     with app.test_client() as client:
         yield client
 
 def test_home(client):
-    response = client.get('/')
-    assert response.data == b"Hello, World!"
-    assert response.status_code == 200
+    """Test the home page."""
+    rv = client.get('/')
+    assert b"Welcome to my Flask App!" in rv.data
+
+def test_about(client):
+    """Test the about page."""
+    rv = client.get('/about')
+    assert b"This is a simple DevOps project to showcase CI/CD and Docker." in rv.data
+
+# # test_app.py
+# import pytest
+# from app import app
+
+# @pytest.fixture
+# def client():
+#     with app.test_client() as client:
+#         yield client
+
+# def test_home(client):
+#     response = client.get('/')
+#     assert response.data == b"Hello, World!"
+#     assert response.status_code == 200
