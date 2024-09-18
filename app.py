9632e2b97e2c315c00ee363fd5238385f14a2e09
Akashdeep singh <akashdeep.singh@noi-dc01.mcarbon.com>
2024-09-18 12:33:35 +0530

    Fix flake8 linting issues

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 
+
 app = Flask(__name__)
 
+
 metrics = PrometheusMetrics(app)
+
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
 
 # Run the application
 if __name__ == '__main__':
