ed9a877ee04f314b030b533f15eb184901f6199e
Akashdeep singh <akashdeep.singh@noi-dc01.mcarbon.com>
2024-10-11 17:05:36 +0530

    pytest and flake8 corrections

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, flash, request
+from flask import Flask, render_template
 from prometheus_flask_exporter import PrometheusMetrics
 # from flask_sqlalchemy import SQLAlchemy
 # from flask_bcrypt import Bcrypt
--- test_app.py
+++ test_app.py
@@ -1,6 +1,5 @@
 import pytest
-from app import app, db, User
-from flask_bcrypt import Bcrypt
+from app import app  # Import your Flask app
 
 
 @pytest.fixture
@@ -10,49 +9,19 @@ def client():
         yield client
 
 
-@pytest.fixture
-def init_database(client):
-    # Create the database and the user for testing within an app context
-    with app.app_context():
-        db.create_all()
-
-        # Create a test user
-        bcrypt = Bcrypt()
-        hashedp = bcrypt.generate_password_hash('test').decode('utf-8')
-        user = User(username='test', email='test@exa.com', password=hashedp)
-        db.session.add(user)
-        db.session.commit()
-
-        yield client
-
-        # Cleanup
-        db.drop_all()
-
-
-def login(client):
-    """Helper function to log in a user"""
-    client.post('/login', data={'email': 'test@exa.com', 'password': 'test'})
-
-
-def test_home(init_database):
+def test_home(client):
     """Test the home page loads successfully."""
-    client = init_database
-    login(client)  # Log in the user
     rv = client.get('/')
     assert rv.status_code == 200
 
 
-def test_about(init_database):
+def test_about(client):
     """Test the about page loads successfully."""
-    client = init_database
-    login(client)  # Log in the user
     rv = client.get('/about')
     assert rv.status_code == 200
 
 
-def test_personal_info(init_database):
+def test_personal_info(client):
     """Test the personal info page loads successfully."""
-    client = init_database
-    login(client)  # Log in the user
     rv = client.get('/personal-info')
     assert rv.status_code == 200
